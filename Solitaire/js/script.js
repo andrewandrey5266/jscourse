c0e88bd4b565ca608c031747be8b31a517546848
adus <adus@telenor.dk>
2019-05-27 17:09:28 +0300

    remove unused code

--- Solitaire/js/script.js
+++ Solitaire/js/script.js
@@ -32,21 +32,18 @@ function Game() {
 	this.$playContainer = document.getElementById('playDecks');
 	this.$container = document.getElementById('game');
 	this.$playContainer.addEventListener("click", cardClick);
-
-
-	this.createDecks();
-	this.registerEvents();
 }
 
 function cardClick(e)
 {
-	
-
 	var selectedCards = document.querySelectorAll(".selected");
 	if(selectedCards.length == 1)
 	{
 		if(selectedCards[0].parentElement != event.target.parentElement
-			&& (event.target.classList.contains("card") || event.target.classList.contains("deck"))){
+			&& (event.target.classList.contains("card") 
+				|| event.target.classList.contains("deck"))){
+
+		console.log(selectedCards[0]);
 
 		selectedCards[0].classList.remove("selected");
 
@@ -67,17 +64,23 @@ function cardClick(e)
 				newParent = event.target.parentNode;
 			}else if (event.target.classList.contains("deck")){
 				newParent = event.target;
-			} else	{
+			}else	{
 				throw new Exception("not implemented");
 			}
 
 			for(var i = 0; i < selectedElements.length; i++){
 				newParent.appendChild(selectedElements[i]);
 			}
+
 			if(selectedParent.lastElementChild != null){
 				selectedParent.lastElementChild.classList.add("open");
 			}
 			return;
+			//check if deck is full
+			//check if game is over / player wins
+		}
+		else{
+			//cannot move modal box
 		}
 	}
 
@@ -90,133 +93,3 @@ function cardClick(e)
 		event.target.classList.add("selected");
 	}
 }
-
-function DealDeck() {
-	Deck.call(this);
-
-	//deal deck must create extra deck node nearby to place opened cards there
-	this.$el.classList.add('flat');
-}
-
-function FinishDeck() {
-	Deck.call(this, 0);
-
-	this.$el.classList.add('flat');
-
-	this.cards = [];
-	this.suit = null;
-}
-
-function PlayingDeck(cardsKit) {
-	Deck.call(this, cardsAmount);
-
-	if (this.cards.length > 1) {
-		this.cards.slice(-1).open();
-	}
-}
-
-function Deck(cardsKit) { // cards kit is array of objects like this [{number: 1, suit: 0, color: 0}, {number: 6, suit: 1, color: 1}]
-	this.cards = [];
-
-	if (cardsKit.length) {
-		this.createCards(cardsKit);
-	}
-
-	//decks must be wrapped in .col div
-	this.$el.classList.add('deck');
-
-	this.registerEvents();
-}
-
-function Card(color, suit, number) {
-	this.color = color;
-	this.suit = suit;
-	this.number = number;
-	this.isOpen = false;
-
-	this.$el = document.createElement('div');
-	this.$el.classList.add('card', GAME_SETTINGS.suitsNames[suit]);
-	this.$el.innerText = GAME_SETTINGS.signs[number];
-
-	this.registerEvents();
-}
-
-Game.prototype = {
-	createDecks: function() {
-		//this.$stashContainer.innerHTML = '';
-		//this.$playContainer.innerHTML = '';
-
-		//create decks here
-		console.log("create decks")
-	},
-
-	getProperFinishDeck: function(suit) {
-		return this.finishDecks.filter((deck) => deck.suit === suit)[0]
-				|| this.finishDecks.filter((deck) => deck.isEmpty())[0];
-	},
-
-	registerEvents: function() {
-
-	},
-}
-
-Deck.prototype = {
-	createCards: function(cardsAmount) {
-
-	},
-
-	registerEvents: function() {
-
-	}
-}
-
-Card.prototype = {
-	select: function() {
-		this.$el.classList.add('selected');
-	},
-
-	unselect: function() {
-		this.$el.classList.remove('selected');
-	},
-
-	open: function() {
-		this.$el.classList.add('open');
-		this.isOpen = true;
-	},
-
-	onClick: function() {
-		this.$el.classList.add('selected');
-		console.log("selected");
-	},
-
-	onDoubleClick: function() {
-
-	},
-
-	registerEvents: function() {
-		this.$el.addEventListener('click', this.onClick.bind(this));
-		this.$el.addEventListener('dblclick', this.onDoubleClick.bind(this));
-
-		console.log("register events");
-	}
-}
-
-DealDeck.prototype = Object.assign(Object.create(Deck.prototype), {
-	onClick: function() {
-
-	}
-});
-
-FinishDeck.prototype = Object.assign(Object.create(Deck.prototype), {
-	isEmpty: function() {
-		return !(this.suit || this.cards.length);
-	},
-
-	setEmpty: function() {
-		this.suit = null;
-	}
-});
-
-PlayingDeck.prototype = Object.assign(Object.create(Deck.prototype), {
-	
-});
