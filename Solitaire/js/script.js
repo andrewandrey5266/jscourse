86d92436c3dbcc7ad8a92d7ab91c23c278711d9e
adus <adus@telenor.dk>
2019-06-14 15:43:55 +0300

    made stash clickable movable

--- Solitaire/js/script.js
+++ Solitaire/js/script.js
@@ -27,69 +27,158 @@ function Game() {
 	this.dealDeck = null;
 	this.finishDecks = [];
 	this.playDecks = [];
-
-	this.$stashContainer = document.getElementById('stashDecks');
+	
+	this.$stashContainer = document.getElementById('stashDecks').getElementsByClassName("col-3")[0];
 	this.$playContainer = document.getElementById('playDecks');
 	this.$container = document.getElementById('game');
+
 	this.$playContainer.addEventListener("click", cardClick);
+	this.$stashContainer.addEventListener("click", stashClick);
 }
 
-function cardClick(e)
+function Card()
 {
-	var selectedCards = document.querySelectorAll(".selected");
-	if(selectedCards.length == 1)
+
+}
+
+Card.prototype = {
+	isSelected: function(card){
+		return card.classList.contains("card") 
+				&& card.classList.contains("open") 
+				&& card.classList.contains("selected");
+	},
+
+	isOpened: function(card){
+		return card.classList.contains("card") 
+				&& card.classList.contains("open") 
+				&& card.classList.contains("selected");
+	}
+}
+
+
+function stashClick()
+{
+	if(event.target.classList.contains("card") && event.target.classList.contains("open") == false)
 	{
-		if(selectedCards[0].parentElement != event.target.parentElement
+		var parent = event.target.parentElement;
+		parent.removeChild(event.target);
+		parent.appendChild(event.target);
+		event.target.classList.add("open");
+	}
+	else if(event.target.classList.contains('deck'))
+	{
+		Array.from(event.target.children).forEach(function(e){
+			e.classList.remove("open");
+		});
+	}
+	else if(event.target.classList.contains("open"))
+	{
+		event.target.classList.toggle("selected");
+	}
+}
+
+function cardClick()
+{
+	var playSelected = document.getElementById('playDecks').querySelectorAll(".selected")[0];
+	var stashSelected = document.getElementById('stashDecks').querySelectorAll(".selected")[0];
+	
+	if(playSelected != undefined)
+	{
+		playSelected.classList.remove("selected");
+		
+		if(playSelected.parentElement != event.target.parentElement
 			&& (event.target.classList.contains("card") 
-				|| event.target.classList.contains("deck"))){
-
-		console.log(selectedCards[0]);
-
-		selectedCards[0].classList.remove("selected");
-
-		var selectedElements = new Array(selectedCards[0]);
-		var selectedParent = selectedCards[0].parentElement;
-		var nextElementSibling = selectedCards[0].nextElementSibling;
-		selectedParent.removeChild(selectedCards[0]);
-
-		while(nextElementSibling != null){
-			selectedElements.push(nextElementSibling);
-			var nextElementSibling1 = nextElementSibling.nextElementSibling;
-			selectedParent.removeChild(nextElementSibling);
-			nextElementSibling = nextElementSibling1;
-		}
-
-			var newParent;
-			if(event.target.classList.contains("card")){
-				newParent = event.target.parentNode;
-			}else if (event.target.classList.contains("deck")){
-				newParent = event.target;
-			}else	{
-				throw new Exception("not implemented");
-			}
-
-			for(var i = 0; i < selectedElements.length; i++){
-				newParent.appendChild(selectedElements[i]);
-			}
-
-			if(selectedParent.lastElementChild != null){
-				selectedParent.lastElementChild.classList.add("open");
-			}
-			return;
+				|| event.target.classList.contains("deck")))
+		{
+			dropDownPlayCard(playSelected);
 			//check if deck is full
 			//check if game is over / player wins
 		}
 		else{
-			//cannot move modal box
+			playSelected.classList.remove("selected");
 		}
 	}
+	else if(stashSelected != undefined){
+		dropDownStashCard(stashSelected);
+	}
+	else
+	{
+		pickUpPlayCard();
+	}
+}
 
-	document.querySelectorAll(".selected").forEach(function(e){
-		e.classList.remove("selected");
-	});
-
+function pickUpPlayCard()
+{
 	if(event.target.classList.contains("open"))
 	{
 		event.target.classList.add("selected");
 	}
+}
+
+function dropDownPlayCard(selectedCard)
+{
+	var selectedElements = new Array(selectedCard);
+	var selectedParent = selectedCard.parentElement;
+	selectedParent.removeChild(selectedCard);
+	var nextElementSibling = selectedCard.nextElementSibling;
+
+	while(nextElementSibling != null)
+	{
+		selectedElements.push(nextElementSibling);
+		var nextElementSibling1 = nextElementSibling.nextElementSibling;
+		selectedParent.removeChild(nextElementSibling);
+		nextElementSibling = nextElementSibling1;
+	}
+
+	var newParent;
+	if(event.target.classList.contains("card"))
+	{
+		newParent = event.target.parentNode;
+	}
+	else if (event.target.classList.contains("deck"))
+	{
+		newParent = event.target;
+	}
+	else	
+	{
+		throw new Exception("not implemented");
+	}
+
+	for(var i = 0; i < selectedElements.length; i++)
+	{
+		newParent.appendChild(selectedElements[i]);
+	}
+	if(selectedParent.lastElementChild != null)
+	{
+		selectedParent.lastElementChild.classList.add("open");
+	}
+}
+
+function dropDownStashCard(selectedCard)
+{
+	var selectedParent = selectedCard.parentElement;
+	selectedParent.removeChild(selectedCard);
+
+	var newParent;
+	if(event.target.classList.contains("card"))
+	{
+		newParent = event.target.parentNode;
+	}
+	else if (event.target.classList.contains("deck"))
+	{
+		newParent = event.target;
+	}
+	else	
+	{
+		throw new Exception("not implemented");
+	}
+
+	newParent.appendChild(selectedCard);
+	selectedCard.classList.remove("selected");
+}
+
+function unselectAll(){
+	document.querySelectorAll(".select").forEach(function(e){
+		e.classList.remove("selected");
+	})
 }
